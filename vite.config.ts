88f78576b412337d90e6205603cf4fed265b7f97
Zein Hammad Marhabah <zein.hammad@sera.astra.co.id>
2025-08-02 17:18:57 +0700

    chore: add core dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  optimizeDeps: {
+    include: ["react-router", "react-router-dom"],
+  },
 });
